b777f0abdf408e30bbeed15e1bd775be9a5b3a86
James Smith <james@foxglove.dev>
2022-10-17 08:27:21 +1100

    rosbag2_performance: add storage plugin comparison
    Adds a package for comparing the write throughput of
    different rosbag2 storage plugins, and provides configurations to
    compare the MCAP and default storage plugins.
    
    Also adds a set of configurations and a shell script in
    rosbag2_compression_comparison to compare the compressed size of bags
    using different compression techniques.
    
    Signed-off-by: James Smith <james@foxglove.dev>

---
+++ rosbag2_performance/rosbag2_storage_plugin_comparison/scripts/plot.py
@@ -0,0 +1,59 @@
+import argparse
+import sys
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas
+from pathlib import Path
+
+LABEL_CATEGORIES = ["plugin_config", "messages", "batch_size"]
+OUTPUT_METRIC = "avg_byte_throughput"
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("csv", help="input CSV to draw plots of")
+    parser.add_argument("--outdir", default="./plots", help="output directory to write plots to")
+    parser.add_argument("--title", help="A title to add to each plot")
+    args = parser.parse_args()
+    Path(args.outdir).mkdir(parents=True, exist_ok=True)
+    frame = pandas.read_csv(args.csv)
+    means = frame.groupby(LABEL_CATEGORIES)[OUTPUT_METRIC].mean().rename("mean")
+    # multiply by 3.92 to turn a std error to 95% confidence interval
+    confidence_intervals = frame.groupby(LABEL_CATEGORIES)[OUTPUT_METRIC].std().rename("ci") * 3.92
+    joined = pandas.concat([means, confidence_intervals], axis=1).sort_values("mean", ascending=False)
+
+
+    message_labels = joined.index.get_level_values(1).unique()
+    batch_labels = joined.index.get_level_values(2).unique()
+    for message_label in message_labels:
+        for batch_label in batch_labels:
+            series = joined[
+                np.logical_and(
+                    joined.index.isin([message_label], level=1),
+                    joined.index.isin([batch_label], level=2)
+                )
+            ]
+            plt.bar(
+                x=series.index.get_level_values(0),
+                height=series["mean"],
+                yerr=series["ci"],
+                align='edge',
+                ecolor='black',
+                color=["skyblue" if name.startswith("mcap") else "aquamarine" for name in series.index.get_level_values(0)],
+                capsize=10,
+            )
+            plt.ylabel("Throughput (bytes/s)")
+            title = f"batch size: {batch_label}, message sizes: {message_label}"
+            if args.title is not None:
+                title = f"{args.title}\n{title}"
+            plt.title(title)
+            plt.tick_params(labelrotation=45)
+            plt.tight_layout()
+            outpath = Path(args.outdir) / f"{message_label}_{batch_label}.png"
+            print(f"saving figure to {outpath}", file=sys.stderr)
+            plt.savefig(outpath)
+            plt.clf()
+
+
+if __name__ == "__main__":
+    main()
